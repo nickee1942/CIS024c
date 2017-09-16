473b8d10cbf34884faca88294a500bef4a76ce50
nickee1942 <nickee1942@gmail.com>
2017-09-15 17:03:52 -0700

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,101 @@
+
+# coding: utf-8
+
+# ### Exercise 3.1
+
+# In[ ]:
+
+a = 5
+b = 6
+
+print a,b
+
+
+# ### Exercise 3.2
+
+# In[6]:
+
+greeting = "Hello "
+person = "Oscar"
+
+
+# ### Exercise 3.3
+
+# In[7]:
+
+personGreeting = greeting + person
+
+print personGreeting
+
+
+# ### Exercise 3.4
+
+# In[1]:
+
+person_age = int(input("Enter age:"))
+
+
+# ### Exercise 3.5
+
+# In[4]:
+
+age = person_age
+
+print age
+
+
+# ### Exercise 3.6 & Exercise 3.7
+
+# In[8]:
+
+if age > 35:
+    print "the age is greater than 35"
+else:
+    print "the age is not greater than 35"
+
+
+# ### Exercise 3.8
+
+# In[38]:
+
+number = 10
+
+print number/4
+print number//4
+
+newNumber1 = number%4
+
+print newNumber1
+
+
+# ### Exercise 3.9
+
+# In[39]:
+
+number = 101.35
+
+print number/4
+print number//4
+
+newNumber2 = number%4
+
+print newNumber2
+
+
+# ### Exercise 3.10
+
+# In[25]:
+
+myAge = 21
+yourAge = 25
+
+result = myAge == yourAge
+
+print result
+
+hisAge = 25
+
+result = myAge == hisAge
+
+print result
+
